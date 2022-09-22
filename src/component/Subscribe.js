6e680304dd64d66318d03a9e6a15ea46e5d04dee
najinp <najinida92@gmail.com>
2022-09-22 17:51:04 +0900

    20220922

--- src/component/Subscribe.js
+++ src/component/Subscribe.js
@@ -7,6 +7,8 @@ const Subscribe = () => {
                 <img className='sub_back' src="assets/img/subscribe.jpg" alt="" />
 
                 <div className="ipb">
+                    <span className='cali'> IN CALIFORNIA SINCE 1948</span>
+
                     <input></input>
                 </div>
             </div>
